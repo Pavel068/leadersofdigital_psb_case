7bee9bd2d06da636fdff8e03f13e0e97b040f50c
Pavel068 <Pavel068>
2021-12-03 02:42:59 +0300

    Fixes

--- config/web.php
+++ config/web.php
@@ -4,6 +4,8 @@ $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
 
 $config = [
+    'language' => 'ru-RU',
+    'sourceLanguage' => 'en-US',
     'id' => 'basic',
     'name' => 'Промсвязьбанк',
     'basePath' => dirname(__DIR__),
