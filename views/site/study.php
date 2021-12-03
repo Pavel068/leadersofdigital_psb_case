8e1372bacdee676e3ac6cc6749a6a74fed4490c3
Pavel068 <Pavel068>
2021-12-03 23:50:19 +0300

    Showing tasks in profile

--- views/site/study.php
+++ views/site/study.php
@@ -2,42 +2,40 @@
 
 /* @var $this yii\web\View */
 
-$this->title = Yii::$app->name;
+$this->title = 'Обучение';
+$this->params['breadcrumbs'][] = $this->title;
+
 ?>
 <div class="site-study">
 
-    <div class="body-content">
-        <div class="card">
-            <div class="card-header">Мои задания</div>
-            <div class="card-body shadow">
-                <?php
-                $tasks = \app\models\Tasks::find()->all();
-                ?>
+    <?php
+    $tasks = \app\models\Tasks::find()->all();
+    ?>
 
-                <div class="tasks">
-                    <?php foreach ($tasks as $task): ?>
-                        <div class="task">
-                            <p class="lead"><?= $task->name ?></p>
-                            <p class="text-muted"><?= $task->description ?></p>
-                            <a href="<?= $task->content_url ?>" target="_blank">Материал</a>
-                            <a class="btn btn-primary" href="">Приступить</a>
-                        </div>
-                    <?php endforeach; ?>
-                </div>
-
-            </div>
-        </div>
-
-    </div>
+    <table class="table table-bordered table-striped">
+        <tr>
+            <th>Название</th>
+            <th>Описание</th>
+            <th>Ссылка</th>
+            <th></th>
+        </tr>
+        <?php foreach ($tasks as $task): ?>
+            <tr>
+                <td><?= $task->name ?></td>
+                <td>
+                    <code class="text-muted" style="width: 250px;">
+                        <?= $task->description ?>
+                    </code>
+                </td>
+                <td>
+                    <?php if ($task->content_url): ?>
+                        <a href="<?= $task->content_url ?>" target="_blank">Материал</a>
+                    <?php endif; ?>
+                </td>
+                <td class="d-flex flex-row">
+                    <a href=""><i class="fas fa-arrow-circle-right"></i></a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
 </div>
-
-<style>
-    .tasks > .task {
-        padding: 5px;
-        border: 1px solid #bebebe;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        margin-top: 10px;
-    }
-</style>
